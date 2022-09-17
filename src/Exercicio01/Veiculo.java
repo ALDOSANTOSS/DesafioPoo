9b94b570ee1810c5c516d0700b8e83544ed1fd9b
aldok <aldo.santos.oliveira95@gmail.com>
2022-09-16 23:43:18 -0300

    Revisando

--- src/Exercicio01/Veiculo.java
+++ src/Exercicio01/Veiculo.java
@@ -11,11 +11,115 @@ public class Veiculo {
     int velocidade;
     double preco;
 
-    void acelerar(){
+    public Veiculo() {
     }
-    void abastecer(){
+
+    public Veiculo(String marca, String modelo, String placa, String cor, float km, boolean ligado, int litrosCombustivel, int velocidade, double preco) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.placa = placa;
+        this.cor = cor;
+        this.km = km;
+        this.ligado = ligado;
+        this.litrosCombustivel = litrosCombustivel;
+        this.velocidade = velocidade;
+        this.preco = preco;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public float getKm() {
+        return km;
+    }
+
+    public void setKm(float km) {
+        this.km = km;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public int getLitrosCombustivel() {
+        return litrosCombustivel;
+    }
+
+    public void setLitrosCombustivel(int litrosCombustivel) {
+        this.litrosCombustivel = litrosCombustivel;
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    void acelerar(){
+        this.velocidade += 20;
+
+    }
+    void abastecer(int Combustivel){
+        if(litrosCombustivel+Combustivel > 60){
+            System.out.println("Nao cabe no tanque");
+        } else {
+            litrosCombustivel += Combustivel;
+            System.out.println("O tanque foi abastecido");
+        }
+
     }
     void frear(){
+        if (velocidade > 0){
+            System.out.println("Nao possivel frear parado");
+        } else {
+            velocidade -= 20;
+            System.out.println("Parando");
+        }
+
+
     }
     void pintar(){
     }
@@ -25,5 +129,6 @@ public class Veiculo {
     void desligar(){
         this.ligado = false;
     }
+
 
 }
--- src/Exercicio01/VeiculoTest01.java
+++ src/Exercicio01/VeiculoTest01.java
@@ -8,7 +8,7 @@ public class VeiculoTest01 {
         Veiculo v1 = new Veiculo();
         System.out.printf("Informe o marca: ");
         v1.marca = scanner.next();
-        // System.out.printf("Marca: "+v1.marca);
+        System.out.printf("Marca: "+v1.marca);
         System.out.printf("Qual o modelo: ");
         v1.modelo = scanner.next();
         System.out.printf("A placa: ");
@@ -17,13 +17,24 @@ public class VeiculoTest01 {
         v1.cor = scanner.next();
         System.out.printf("Quantos Km? ");
         v1.km = scanner.nextFloat();
-        // System.out.printf("ligado");
+        //System.out.printf("ligado");
         System.out.printf("Quantos litros Combustivel? ");
         v1.litrosCombustivel = scanner.nextInt();
         System.out.printf("Qual a velocidade? ");
         v1.velocidade = scanner.nextInt();
         System.out.printf("Qual o preco? ");
         v1.preco = scanner.nextDouble();
+        System.out.println("Marca: "+v1.marca);
+        System.out.println("Modelo: "+v1.modelo);
+        System.out.println("Placa: "+v1.placa);
+        System.out.println("Cor: "+v1.cor);
+        System.out.println("Km: "+v1.km);
+        System.out.println("ligado: "+v1.ligado);
+        System.out.println("Quantos litros Combustivel"+v1.litrosCombustivel);
+        System.out.println("Velocidade: "+v1.velocidade);
+        System.out.println("Preco: "+v1.preco);
+        v1.abastecer(scanner.nextInt());
+
 
 
 
